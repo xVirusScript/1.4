395e922666c354e76de2d61d6ad8b6d88a347df5
xVirusScript <42717524+xVirusScript@users.noreply.github.com>
2018-09-07 20:27:39 +0700

    Create One click SD 425.lua

---
+++ One click SD 425.lua
@@ -0,0 +1,206 @@
+
+xRICx = 1
+function xRICx()
+  Menu = gg.choice({
+    "⟬///////⟭ㄖOne Click〃",
+    "⟬EXIT⟭",
+  },nil,'☣PUBGM HACK 0.7.5丷RIC 1.4〃\n♕YouTube : RIC 2210〃')
+  if Menu == 1 then bitch() end
+  if Menu == 2 then EXIT() end
+  RIC2210 = -1
+end
+
+
+function bitch()
+  gg.setVisible(false)
+  XD = gg.alert(" ⌬「RIC 2210 HACK PUBGM 0.7.5」⌬\n ONE CLICK LOBBY LANDING","LOBBY","LANDING")
+  if XD == 1 then AHS() hlc() NRC() smc() end
+  if XD == 2 then wh425() xGren() BS() rgs() LSTa() ATAW() SP() end
+RIC2210 = -1
+end
+
+-----LOBBY
+
+function AHS()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("9.201618;30.5;25", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("30.5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(10)
+  gg.editAll("250", gg.TYPE_FLOAT)
+  gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("9.201618;30.5;25", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("25", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL,  0, -1)
+gg.getResults(10)
+gg.editAll("522", gg.TYPE_FLOAT)
+gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("12.66705417633;28;15;26:5120", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(10)
+  gg.editAll("250", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.setRanges(gg.REGION_C_BSS)
+gg.searchNumber("2048D;1F",gg.TYPE_FLOAT,false,gg.SIGN_FUZZY_EQUAL,0,-1)
+gg.searchNumber("1",gg.TYPE_FLOAT,false,gg.SIGN_FUZZY_EQUAL,0,-1)
+var = gg.getResults(100)
+gg.editAll("0.07",gg.TYPE_FLOAT)
+  gg.clearResults()
+gg.toast("╚╔Auto Headshot 😰 95% Activated╗╝")
+end
+
+function hlc()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+gg.clearResults()
+gg.searchNumber("3.5;1;200;20::999",gg.TYPE_FLOAT,false,gg.SIGN_FUZZY_EQUAL,0,-1)
+gg.searchNumber("3.5;1;200;20",gg.TYPE_FLOAT,false,gg.SIGN_FUZZY_EQUAL,0,-1)
+var = gg.getResults(300)
+gg.editAll("-1.0e10",gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("╚╔HeadLock Aimbot Activated╗╝")
+end
+
+
+function NRC() 
+    gg.clearResults()
+gg.searchNumber("1868784978;1850305641;28518", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("1868784978", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(1000)
+gg.editAll("1868756421", gg.TYPE_DWORD)
+gg.clearResults() 
+gg.searchNumber("1750294898;1415932769;1819307365", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1) 
+gg.searchNumber("1750294898;1415932769;1819307365", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(1000)
+gg.editAll("100000", gg.TYPE_DWORD)
+gg.clearResults()
+    gg.clearResults()
+    gg.toast("╚╔No Recoil Activated╗╝")
+  end
+
+function smc()
+  gg.setRanges(32)
+  gg.searchNumber("3.20000004768;1.09375", 16, false, 536870912, 0, -1)
+  gg.searchNumber("3.20000004768;1.09375", 16, false, 536870912, 0, -1)
+  gg.getResults(100)
+  gg.editAll("0", 16)
+  gg.toast("╚╔Small Croshair Activated╗╝")
+end
+
+
+---LANDING
+
+function wh425() --- I bet you will leaked this script for snap 425 and tell its yours😆
+  gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("1,073,741,824;227;1,073,741,824;-1,082,130,432;1,073,741,824:29", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1,073,741,824", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(100)
+  gg.editAll("1,125,515,264", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("19;15;12;15;16;11;7;20;7;23;27;15;26;23;1,073,741,824", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1,073,741,824", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(100)
+  gg.editAll("1,125,515,264", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.clearResults()
+     gg.toast("╚╔Wallhack Snap 425 Activated╗╝")
+  end
+  
+  function xGren() 
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("1,080,033,303;589,831;1,080,051,971:17",gg.PROT_EXEC,false,gg.SIGN_FUZZY_EQUAL,0,-1)
+gg.searchNumber("589,831",gg.PROT_EXEC,false,gg.SIGN_FUZZY_EQUAL,0,-1)
+var = gg.getResults(100)
+gg.editAll("589,840",gg.PROT_EXEC)
+gg.clearResults()
+gg.searchNumber("1,080,051,971;589,847;1,080,049,669:9",gg.PROT_EXEC,false,gg.SIGN_FUZZY_EQUAL,0,-1)
+gg.searchNumber("589,847",gg.PROT_EXEC,false,gg.SIGN_FUZZY_EQUAL,0,-1)
+var = gg.getResults(10)
+gg.editAll("-1",gg.PROT_EXEC,false,gg.SIGN_FUZZY_EQUAL,0,-1)
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("1,280;9;-2,144,337,911;589,830;1,081,104,896:21",gg.PROT_EXEC,false,gg.SIGN_FUZZY_EQUAL,0,-1)
+gg.searchNumber("589,830",gg.PROT_EXEC,false,gg.SIGN_FUZZY_EQUAL,0,-1)
+var = gg.getResults(2)
+gg.editAll("10",gg.PROT_EXEC)
+gg.toast("╚╔Collors GREEN Activated╗╝")
+end 
+
+function BS()
+gg.clearResults()
+  gg.setRanges(gg.REGION_BAD)
+  gg.searchNumber("2;10000", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(10)
+  gg.editAll("0", gg.TYPE_FLOAT, FREEZE_NORMAL)
+  gg.clearResults()
+  gg.setRanges(gg.REGION_BAD)
+  gg.searchNumber("4.8883906e21", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("4.8883906e21", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(100)
+  gg.editAll("4.8883906e13", gg.TYPE_FLOAT)
+gg.toast("╚╔Black Sky SD 400+ Activated╗╝")
+end
+  
+  function rgs()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("8.0F;1.20000004768F;0.80000001192F;1.5F;0.80000001192F;1.5F::512", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("8.0", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(300)
+gg.editAll("0", gg.TYPE_FLOAT)
+gg.toast("╚╔Remove Grass Activated╗╝")
+end
+
+function LSTa()
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("1,092,081,726;1,003,658,240;923,795,456", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1,092,081,726", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(100)
+  gg.editAll("1,136,081,726", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.toast("╚╔Lift Sit Down Aim Activated╗╝")
+end
+  
+  function ATAW()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.clearResults()
+  gg.searchNumber("0.53446006775F;-1.68741035461F:501", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("-1.68741035461", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(1995)
+  gg.editAll("19995", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.searchNumber("18.38612365723F;0.54026412964F:5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1) 
+  gg.searchNumber("18.38612365723F;0.54026412964F:5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(1995)
+  gg.editAll("19995", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.toast("╚╔Antena Always Activated╗╝")
+  end
+
+  function SP()
+gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber('1;1;1;0.0001;20;0.0005;0.4::50', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber('1', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(500)
+  gg.editAll('1.04', gg.TYPE_FLOAT)
+  gg.clearResults()
+gg.toast("╚╔Speed Hack Activated╗╝")
+end
+
+
+
+function EXIT()
+gg.toast("www.youtube.com/c/RIC 2210")
+os.exit()
+end
+  
+cs = "Copyright By RIC 2210"
+while true do
+if gg.isVisible(true) then
+RIC2210 = 1
+gg.setVisible(false) 
+end
+gg.clearResults()
+if RIC2210 == 1 then xRICx() end
+end
+
